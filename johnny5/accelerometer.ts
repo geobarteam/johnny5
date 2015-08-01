30e9fd6819e253088a3e820891f3b80c1821f236
Geoffrey <gvd8@hotmail.com>
2015-08-01 15:01:33 +0200

    commit missing files

---
+++ johnny5/accelerometer.ts
@@ -0,0 +1,63 @@
+﻿///<reference path=".\lib\node.d.ts"/>
+///<reference path=".\lib\johnny-five.d.ts"/>
+
+import five = require('johnny-five');
+import Emitter = require('events');
+
+export class AccelerometerData {
+    public x: number;
+    public y: number;
+    public z: number;
+    public pitch: number;
+    public roll: number;
+    public acceleration: number;
+    public inclination: number;
+    public orientation: number;
+}
+
+export interface IAccelerometer extends NodeJS.EventEmitter {
+    getData(): AccelerometerData;
+}
+
+export class AccelerometerOption implements five.AccelerometerMMA7361Option {
+    controller = "MMA7361";
+    pins = ["A0", "A1", "A2"];
+    sleepPin = 10;
+    autoCalibrate = true;
+}
+
+export class Accelerometer extends Emitter.EventEmitter implements IAccelerometer {
+    accelerometer: five.Accelerometer;
+    board: five.Board;
+    data: AccelerometerData;
+    option: five.AccelerometerMMA7361Option;
+
+    constructor(board: five.Board, option: five.AccelerometerMMA7361Option) {
+        this.board = board;
+        this.option = option;
+        this.data = new AccelerometerData();
+        var that = this;
+        this.board.on("ready", function () {
+            that.accelerometer = new five.Accelerometer(that.option);
+            that.accelerometer.on("change", function () {
+                that.data.acceleration = this.acceleration;
+                that.data.inclination = this.inclination;
+                that.data.orientation = this.orientation;
+                that.data.x = this.x;
+                that.data.y = this.y;
+                that.data.z = this.z;
+                that.data.pitch = this.pitch;
+                that.data.roll = this.roll;
+                that.emit("change", that.data)
+            });
+        })
+
+        super();
+    }
+
+    public getData(): AccelerometerData {
+        return this.data;
+    }
+
+}
+
---
+++ johnny5/radar.ts
@@ -0,0 +1,75 @@
+﻿///<reference path=".\lib\node.d.ts"/>
+///<reference path=".\lib\johnny-five.d.ts"/>
+
+import five = require('johnny-five');
+import Emitter = require('events');
+
+export class RadarData {
+    constructor(
+        public LeftValue: number,
+        public MiddleValue: number,
+        public RightValue: number) {
+    }
+}
+
+export interface IRadar extends NodeJS.EventEmitter {
+    getData(): RadarData;
+    initializeHandler(): any;
+}
+
+export class Radar extends Emitter.EventEmitter implements IRadar {
+
+    radarData: RadarData;
+    leftPing: any;
+    middlePing: any;
+    rightPing: any;
+    board: five.Board;
+
+    public constructor(board: five.Board) {
+        this.radarData = new RadarData(0, 0, 0);
+        this.board = board;
+        console.log(this.board);
+        this.board.on("ready", this.initializeHandler());
+        super();
+    }
+
+    getData(): RadarData {
+        return this.radarData;
+    }
+
+    public initializeHandler() {
+        var radar = this;
+
+        return function () {
+            console.log('Arduino connected');
+            for (var i = 5; i <= 8; i++) {
+                radar.board.pinMode(i, PinMode.OUTPUT);
+            }
+
+            radar.leftPing = new five.Ping(11);
+            radar.middlePing = new five.Ping(12);
+            radar.rightPing = new five.Ping(13);
+
+            radar.leftPing.on("change", function () {
+                radar.radarData.LeftValue = this.cm;
+                radar.emit("change", radar.radarData);
+            });
+            radar.middlePing.on("change", function () {
+                radar.radarData.MiddleValue = this.cm;
+                radar.emit("change", radar.radarData);
+            });
+            radar.rightPing.on("change", function () {
+                radar.radarData.RightValue = this.cm;
+                radar.emit("change", radar.radarData);
+            });
+        }
+    }
+}
+
+export class PinMode {
+    static INPUT = 0;
+    static OUTPUT = 1;
+    static ANALOG = 2;
+    static PWM = 3;
+    static SERVO = 4;
+}
